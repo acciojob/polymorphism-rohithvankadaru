e546688d1152f00140b4c736852d7798f9a5c08f
rohithvankadaru <rohithvankadaru405@gmail.com>
2023-03-23 19:06:42 +0530

    Corrected

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,14 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Product p = new Product();
         
-        int ans1 = p.product(5,10);
-        int ans2 = p.product(7,9,1);
-        double ans3 = p.product(1.2,0.2);
+        int ans = p.product(5,10);
+        int ans1 = p.product(7,9,1);
+        double ans2 = p.product(1.2,0.2);
         
-        System.out.print(ans1+" "+ans2+" "+ans3);
+        System.out.print(ans+" "+ans1+" "+ans2);
     }
 
-    static class Product{
+    public static class Product{
         public int product(int x, int y) {
             return x*y;
         }
